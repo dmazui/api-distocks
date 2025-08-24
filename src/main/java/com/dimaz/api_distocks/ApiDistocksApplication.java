d1ac3b2ae4f9eda044068a3be3af3adf3be97721
DMazui <dev.diogo@gmail.com>
2025-08-23 22:50:22 -0300

    first

---
+++ src/main/java/com/dimaz/api_distocks/ApiDistocksApplication.java
@@ -0,0 +1,13 @@
+package com.dimaz.api_distocks;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApiDistocksApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ApiDistocksApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/dimaz/api_distocks/ApiDistocksApplicationTests.java
@@ -0,0 +1,13 @@
+package com.dimaz.api_distocks;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiDistocksApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

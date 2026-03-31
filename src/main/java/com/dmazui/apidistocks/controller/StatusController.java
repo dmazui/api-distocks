9e7b92857f005236d0dbed4e6938d797a7d9fbe0
DMazui <dev.diogo@gmail.com>
2026-03-30 23:47:39 -0300

    versionamento

--- src/main/java/com/dmazui/apidistocks/controller/StatusController.java
+++ src/main/java/com/dmazui/apidistocks/controller/StatusController.java
@@ -7,7 +7,7 @@ import org.springframework.web.bind.annotation.*;
 public class StatusController {
 
     @GetMapping
-    public String hello() {
+    public String status() {
         return "api-DiStocks is online!";
     }
 }
